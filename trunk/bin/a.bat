64ba2bd9f1c316a7b279b5feccd0eb8178e83d00
riccardoaranha <riccardoaranha@45c4280c-64dc-11de-9f54-6d4fae1110e3>
2009-06-30 18:54:02 +0000

    Full tests script done.

--- trunk/bin/a.bat
+++ trunk/bin/a.bat
@@ -1,11 +1,20 @@
 @echo off
+set Step=0.01
+set MaxTime=20000
+set EPS=0001
 
+set x=10.0
+set y=20.0
+set z=30.0
+
+@echo Max Time = %MaxTime%
+@echo Step     = %Step%
+@echo EPS      = 0.0%EPS%
+
+REM ---------------------------------------------------------------------------------------------
 REM Test 1
 
 IF NOT EXIST test1 mkdir test1
-set x=1.0
-set y=1.0
-set z=1.0
 
 @echo Test1: Same initial condition
 @echo x = %x%
@@ -13,20 +22,20 @@ set z=1.0
 @echo z = %z%
 
 
-lorenzAttractor --euler -x %x% -y %y% -z %z% > test1/euler.plt
-lorenzAttractor --heun  -x %x% -y %y% -z %z% > test1/heun.plt
-lorenzAttractor --rk    -x %x% -y %y% -z %z% > test1/rk.plt
+lorenzAttractor --euler -s %Step% -t %MaxTime% -x %x% -y %y% -z %z% > test1/euler.dat
+lorenzAttractor --heun  -s %Step% -t %MaxTime% -x %x% -y %y% -z %z% > test1/heun.dat
+lorenzAttractor --rk    -s %Step% -t %MaxTime% -x %x% -y %y% -z %z% > test1/rk.dat
 
-
+REM ---------------------------------------------------------------------------------------------
 REM Test 2
 
 IF NOT EXIST test2 mkdir test2
-set xa=1.0
-set ya=1.0
-set za=1.0
-set xb=1.000001
-set yb=1.0
-set zb=1.0
+set xa=%x%
+set ya=%y%
+set za=%z%
+set xb=%x% + %EPS%
+set yb=%y%
+set zb=%z%
 
 @echo Test2: Same Methods, epsilon-diff x initial condition
 @echo x1 = %xa%
@@ -38,23 +47,23 @@ set zb=1.0
 @echo z2 = %zb%
 
 
-lorenzAttractor --euler -x %xa% -y %ya% -z %za% > test2/euler_a.plt
-lorenzAttractor --euler -x %xb% -y %yb% -z %zb% > test2/euler_b.plt
-lorenzAttractor --heun  -x %xa% -y %ya% -z %za% > test2/heun_a.plt
-lorenzAttractor --heun  -x %xb% -y %yb% -z %zb% > test2/heun_b.plt
-lorenzAttractor --rk    -x %xa% -y %ya% -z %za% > test2/rk_a.plt
-lorenzAttractor --rk    -x %xb% -y %yb% -z %zb% > test2/rk_b.plt
+lorenzAttractor --euler -s %Step% -t %MaxTime% -x %xa% -y %ya% -z %za% > test2/euler_a.dat
+lorenzAttractor --euler -s %Step% -t %MaxTime% -x %xb% -y %yb% -z %zb% > test2/euler_b.dat
+lorenzAttractor --heun  -s %Step% -t %MaxTime% -x %xa% -y %ya% -z %za% > test2/heun_a.dat
+lorenzAttractor --heun  -s %Step% -t %MaxTime% -x %xb% -y %yb% -z %zb% > test2/heun_b.dat
+lorenzAttractor --rk    -s %Step% -t %MaxTime% -x %xa% -y %ya% -z %za% > test2/rk_a.dat
+lorenzAttractor --rk    -s %Step% -t %MaxTime% -x %xb% -y %yb% -z %zb% > test2/rk_b.dat
 
-
+REM ---------------------------------------------------------------------------------------------
 REM Test 3
 
 IF NOT EXIST test3 mkdir test3
-set xa=1.0
-set ya=1.0
-set za=1.0
-set xb=1.0
-set yb=1.000001
-set zb=1.0
+set xa=%x%
+set ya=%y%
+set za=%z%
+set xb=%x%
+set yb=%y% + %EPS%
+set zb=%z%
 
 @echo Test3: Same Methods, epsilon-diff y initial condition
 @echo x1 = %xa%
@@ -66,9 +75,37 @@ set zb=1.0
 @echo z2 = %zb%
 
 
-lorenzAttractor --euler -x %xa% -y %ya% -z %za% > test3/euler_a.plt
-lorenzAttractor --euler -x %xb% -y %yb% -z %zb% > test3/euler_b.plt
-lorenzAttractor --heun  -x %xa% -y %ya% -z %za% > test3/heun_a.plt
-lorenzAttractor --heun  -x %xb% -y %yb% -z %zb% > test3/heun_b.plt
-lorenzAttractor --rk    -x %xa% -y %ya% -z %za% > test3/rk_a.plt
-lorenzAttractor --rk    -x %xb% -y %yb% -z %zb% > test3/rk_b.plt
+lorenzAttractor --euler -s %Step% -t %MaxTime% -x %xa% -y %ya% -z %za% > test3/euler_a.dat
+lorenzAttractor --euler -s %Step% -t %MaxTime% -x %xb% -y %yb% -z %zb% > test3/euler_b.dat
+lorenzAttractor --heun  -s %Step% -t %MaxTime% -x %xa% -y %ya% -z %za% > test3/heun_a.dat
+lorenzAttractor --heun  -s %Step% -t %MaxTime% -x %xb% -y %yb% -z %zb% > test3/heun_b.dat
+lorenzAttractor --rk    -s %Step% -t %MaxTime% -x %xa% -y %ya% -z %za% > test3/rk_a.dat
+lorenzAttractor --rk    -s %Step% -t %MaxTime% -x %xb% -y %yb% -z %zb% > test3/rk_b.dat
+
+REM ---------------------------------------------------------------------------------------------
+REM Test 4
+
+IF NOT EXIST test4 mkdir test4
+set xa=%x%
+set ya=%y%
+set za=%z%
+set xb=%x%
+set yb=%y%
+set zb=%z% + %EPS%
+
+@echo Test4: Same Methods, epsilon-diff z initial condition
+@echo x1 = %xa%
+@echo y1 = %ya%
+@echo z1 = %za%
+
+@echo x2 = %xb%
+@echo y2 = %yb%
+@echo z2 = %zb%
+
+
+lorenzAttractor --euler -s %Step% -t %MaxTime% -x %xa% -y %ya% -z %za% > test4/euler_a.dat
+lorenzAttractor --euler -s %Step% -t %MaxTime% -x %xb% -y %yb% -z %zb% > test4/euler_b.dat
+lorenzAttractor --heun  -s %Step% -t %MaxTime% -x %xa% -y %ya% -z %za% > test4/heun_a.dat
+lorenzAttractor --heun  -s %Step% -t %MaxTime% -x %xb% -y %yb% -z %zb% > test4/heun_b.dat
+lorenzAttractor --rk    -s %Step% -t %MaxTime% -x %xa% -y %ya% -z %za% > test4/rk_a.dat
+lorenzAttractor --rk    -s %Step% -t %MaxTime% -x %xb% -y %yb% -z %zb% > test4/rk_b.dat
